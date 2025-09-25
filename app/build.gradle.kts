d7c733b299752ab7280fb581c6438444524bb198
rhpark <cli7942lic@gmail.com>
2025-09-25 15:32:01 +0900

    Remove
     - build.gradle.kts 의 "com.github.rhpark:Android_Custom_Logcat:0.9.2"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.rhpark:Android_Custom_Logcat:0.9.2")
 }
