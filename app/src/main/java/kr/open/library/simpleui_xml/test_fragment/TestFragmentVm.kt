ad77591f26f3761c1252875fa96d119662145401
RhPark <cli7942lic@gmail.com>
2026-07-07 08:11:23 +0900

    - 필요 없는 파일 제거

--- app/src/main/java/kr/open/library/simpleui_xml/test_fragment/TestFragmentVm.kt
+++
@@ -1,5 +0,0 @@
-package kr.open.library.simpleui_xml.test_fragment
-
-import kr.open.library.simple_ui.core.viewmodel.BaseViewModelEvent
-
-class TestFragmentVm : BaseViewModelEvent<TestFragmentVmEvent>()
--- app/src/main/java/kr/open/library/simpleui_xml/test_fragment/TestFragmentVmEvent.kt
+++
@@ -1,5 +0,0 @@
-package kr.open.library.simpleui_xml.test_fragment
-
-sealed interface TestFragmentVmEvent {
-    data object Dump : TestFragmentVmEvent
-}
